b398a2ddebde73fec243c8ba52ea420f89eabf69
Ariq Syahalam <70618885+ariqsyahalam@users.noreply.github.com>
2024-08-13 15:26:54 +0700

    Init

---
+++ TheSpecialKiwi/Models/PlayerModel.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerModel.swift
+//  TheSpecialKiwi
+//
+//  Created by Reyhan Ariq Syahalam on 13/08/24.
+//
+
+import Foundation
---
+++ TheSpecialKiwi/Scenes/EyeContactStage/EyeContactStageView.swift
@@ -0,0 +1,18 @@
+//
+//  EyeContactStageView.swift
+//  TheSpecialKiwi
+//
+//  Created by Reyhan Ariq Syahalam on 13/08/24.
+//
+
+import SwiftUI
+
+struct EyeContactStageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EyeContactStageView()
+}
