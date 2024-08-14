39735c69b371e8348e8debcb04e6a57f0438a315
rayhanjamesc <rayhanjamesc@gmail.com>
2024-08-14 11:59:42 +0700

    Changes: - Added NavigationView, BackButtonModel, CommunicationButtonModel, EyeContactButtonModel, and NavigationViewModel files - Implemented navigateToGame and goBack functions - Implemented needed elements for NavigationView - Integrated NavigationView to TheSpecialKiwiApp

---
+++ TheSpecialKiwi/ViewModels/NavigationViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  NavigationViewModel.swift
+//  TheSpecialKiwi
+//
+//  Created by James Cellars on 14/08/24.
+//
+
+import Foundation
+import SwiftUI
+
+enum Game {
+    case communication
+    case eyeContact
+}
+
+class NavigationViewModel: ObservableObject {
+    @Published var currentGame: Game?
+    @Published var isBackButtonVisible: Bool = false
+    
+    func navigateToGame(_ game: Game) {
+        currentGame = game
+        isBackButtonVisible = true
+    }
+    
+    func goBack() {
+        currentGame = nil
+        isBackButtonVisible = false
+    }
+}
