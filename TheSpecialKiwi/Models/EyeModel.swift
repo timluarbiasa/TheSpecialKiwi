584da974eeb2b79ebb20bb82ac597b84d08229e8
rayhanjamesc <rayhanjamesc@gmail.com>
2024-08-14 09:47:44 +0700

    Changes: - Added ArrowModel, GaugeModel, CommunicationGameView, and CommunicationGameViewModel files - Implemented a mutating function in ArrowModel to change positions of the arrow and reset position function to reset the game - Implemented a check result function in GaugeModel to check whether the arrow stopped at the correct position or not - Implemented startArrow, stopArrow, updateArrowPosition, checkResult, and resetArrow functions to CommunicationGameViewModel

---
+++ TheSpecialKiwi/Models/EyeModel.swift
@@ -0,0 +1,8 @@
+//
+//  EyeModel.swift
+//  TheSpecialKiwi
+//
+//  Created by James Cellars on 14/08/24.
+//
+
+import Foundation
---
+++ TheSpecialKiwi/Models/GaugeModel.swift
@@ -0,0 +1,29 @@
+//
+//  GaugeModel.swift
+//  TheSpecialKiwi
+//
+//  Created by James Cellars on 14/08/24.
+//
+
+import Foundation
+import SwiftUI
+
+struct GaugeModel {
+    let totalHeight: CGFloat = 300
+    
+    var greenZoneTop: CGFloat {
+        totalHeight / 3
+    }
+    
+    var greenZoneBottom: CGFloat {
+        2 * totalHeight / 3
+    }
+    
+    func checkResult(arrowPosition: CGFloat) -> String {
+        if arrowPosition > greenZoneTop && arrowPosition < greenZoneBottom {
+            return "You Win!"
+        } else {
+            return "You Lose!"
+        }
+    }
+}
